7577b4d4716d94408046b7b3930dbd8a8193fd80
Lighter-z <1106606588@qq.com>
2020-02-06 17:34:45 +0800

    类模板分文件编写问题

--- day04/09类模板的分文件编写问题以及解决/main.cpp
+++ day04/09类模板的分文件编写问题以及解决/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+//使用Person.h 会出现报错 两个无法解析的外部命令
 //#include "Person.h"
-#include "Person.cpp"  
+
+
+//#include "Person.cpp"  
 //解决办法  屏蔽 .h
 //或者 添加.hpp文件  Person.cpp 文件中全部删除
 
 
-//#include "Person.hpp" 
+#include "Person.hpp" 
 //建议模板不要做分文件编写
 
 using namespace std;
